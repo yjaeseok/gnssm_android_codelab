6dfcc4afdef7a941c09c9367c3ec9d5be88807e3
Jake Yoon <yjaeseok@gmail.com>
2014-03-23 22:30:02 +0900

    Day1 완료

---
+++ source/Day1-Layout/src/com/example/day1_layout/RelativeLayout.java
@@ -0,0 +1,14 @@
+package com.example.day1_layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class RelativeLayout extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.relativelayout);
+	}
+}

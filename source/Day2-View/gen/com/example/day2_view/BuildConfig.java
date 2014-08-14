29829514e0c8d7dbadf5f5354e59aa70a9a7962e
Jake Yoon <yjaeseok@gmail.com>
2014-08-15 01:30:55 +0900

    Kakao

--- source/Day2-View/gen/com/example/day2_view/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.day2_view;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
---
+++ source/KakaoTalk/src/com/ssm/kakao/KakaoTalkMain.java
@@ -0,0 +1,13 @@
+package com.ssm.kakao;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class KakaoTalkMain extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
